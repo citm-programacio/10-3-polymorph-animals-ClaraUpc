0d7ce518f863bdb261bd39b64a7921642256f39f
clarasa1 <clarasa1@TR12-201-19>
2024-11-20 13:08:46 +0100

    hecho

--- 10-3-polymorph-animals/10-3-polymorph-animals.cpp
+++ 10-3-polymorph-animals/10-3-polymorph-animals.cpp
@@ -1,19 +1,55 @@
 // 10-3-polymorph-animals.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
+//Make a class “Animal” with the abstract method speak()
+//
+//Make classes “dog”and “cat” that inherit from animal.
+//
+//Override the speak method
+//
+//Make the objectsand call speak from main
+
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+class Animal {
+public:
+    
+    virtual void speak() const = 0;
+    virtual ~Animal() {}
+};
+
+
+class Dog : public Animal {
+public:
+    void speak() const override {
+        cout << "Woof! Woof!" << endl;
+    }
+};
+
+
+class Cat : public Animal {
+public:
+    void speak() const override {
+        cout << "Meow! Meow!" << endl;
+    }
+};
+
+int main() {
+
+    Dog dog;
+    Cat cat;
+
+
+    vector<Animal*> animals = { &dog, &cat };
+    for (const auto& animal : animals) {
+        animal->speak();
+    }
+
+    return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
